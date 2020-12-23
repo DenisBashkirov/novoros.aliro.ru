74cda33e3c3c27f832ef048b6fb0b3d881714ba7
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-23 15:28:46 +0300

    Update footer.blade.php

--- resources/views/frontend/sections/footer.blade.php
+++ resources/views/frontend/sections/footer.blade.php
@@ -3,23 +3,22 @@
     <div class="container flex xs-sm:flex-col xl:flex-row xl:justify-between xs-sm:sy-6">
 
         <div class="xs-sm:sy-2 xl:sy-4">
-            <h2 class="xs-sm:text-xl xl:text-2xl font-bold">Офисы продаж</h2>
+            <p class="xs-sm:text-xl xl:text-2xl font-bold">Офисы продаж</p>
             <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
-                <p class="js-goto" data-goto="Krasnodar">{{ $company_contacts['address']['office']['krasnodar'] }}</p>
-                <a href="tel:{{ $company_contacts['phone']['office']['krasnodar'] }}" class="no-underline">{{ $company_contacts['phone']['office']['krasnodar'] }}</a>
+                <p>г. Новороссийск, ул. Героев-Десантников, 2</p>
+                <a href="tel:{{ $company_contacts['phone']['novoros'] }}" class="no-underline">{{ $company_contacts['phone']['novoros'] }}</a>
             </div>
             <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
-                <p class="" data-goto="Sochi">{{ $company_contacts['address']['office']['novoros'] }}</p>
+                <p>г. Краснодар, ул. Калинина, 258</p>
             </div>
             <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
-                <p class="" data-goto="Sochi">{{ $company_contacts['address']['office']['sochi'] }}</p>
+                <p>г. Сочи, ул. Транспортная, 5</p>
             </div>
         </div>
-
         <div class="xs-sm:sy-2 xl:sy-4">
-            <h2 class="xs-sm:text-xl xl:text-2xl font-bold">Производство</h2>
+            <p class="xs-sm:text-xl xl:text-2xl font-bold">Производство</p>
             <div class="flex flex-col xs-sm:sy-1 xl:sy-2 xl:text-xl">
-                <p>{{ $company_contacts['address']['production'] }}</p>
+                <p>п. Яблоновский, Шапсугское ш., 3/15</p>
             </div>
         </div>
 
